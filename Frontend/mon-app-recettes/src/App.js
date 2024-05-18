b993acdd66c388318413b000204549120f2af18f
youssefEL69 <youssef.elmadanipro@gmail.com>
2024-05-19 01:28:18 +0200

    Update : navbar + image/description des recettes

--- Frontend/mon-app-recettes/src/App.js
+++ Frontend/mon-app-recettes/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import CustomNavbar from "./components/Navbar";
 import Home from "./components/Home";
 import ListeRecettes from "./components/ListeRecettes";
 import Recette from "./components/Recette";
 import "./App.scss";
 import Footer from "./components/Footer";
+import CustomNavbar from "./components/Header/Navbar";
 
 function App() {
   return (
--- Frontend/mon-app-recettes/src/components/Header/Navbar.js
+++ Frontend/mon-app-recettes/src/components/Header/Navbar.js
@@ -1,13 +1,16 @@
 import React from "react";
+import "./Navbar.scss"
 import { NavLink, useLocation } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 
+
+
 function CustomNavbar() {
   const location = useLocation();
   return (
-    <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+    <Navbar collapseOnSelect expand="lg" bg="blue" variant="light" className="custom-navbar">
     <Container>
       <Navbar.Brand as={NavLink} to="/" className={location.pathname !== "/" ? "active" : ""}>LyonEat</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -15,11 +18,14 @@ function CustomNavbar() {
         <Nav className="me-auto">
           <Nav.Link as={NavLink} to="/" exact activeClassName="active">Accueil</Nav.Link>
           <Nav.Link as={NavLink} to="/recettes" activeClassName="active">Liste des recettes</Nav.Link>
+          <Nav.Link as={NavLink} to="/categories" activeClassName="active">Recettes par catégories</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Container>
   </Navbar>
   );
 }
+
+
 
 export default CustomNavbar;
